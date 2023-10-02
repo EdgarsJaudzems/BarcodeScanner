70e7cbaa995841e1f1aea7888dcbb359cbdac3c5
Edgars Jaudzems <73849363+EdgarsJaudzems@users.noreply.github.com>
2023-10-02 14:00:25 +0300

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by edgars.jaudzems on 02/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}
