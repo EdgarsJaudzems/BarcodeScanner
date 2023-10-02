9e1629591b936dbad3c995556693f3fe0543f22f
Edgars Jaudzems <73849363+EdgarsJaudzems@users.noreply.github.com>
2023-10-02 16:07:23 +0300

    Add scannerView

---
+++ BarcodeScanner/ScannerView.swift
@@ -0,0 +1,37 @@
+//
+//  ScannerView.swift
+//  BarcodeScanner
+//
+//  Created by edgars.jaudzems on 02/10/2023.
+//
+
+import SwiftUI
+
+struct ScannerView: UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: Context) -> ScannerVC {
+        ScannerVC(scannerDelegate: context.coordinator)
+    }
+    
+    func updateUIViewController(_ uiViewController: ScannerVC, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> Coordinator {
+       Coordinator()
+    }
+    
+    final class Coordinator: NSObject, ScannerVCDelegate {
+        func didFind(barcode: String) {
+            print(barcode)
+        }
+        
+        func didSurface(error: CameraError) {
+            print(error.rawValue)
+        }
+    }
+}
+
+#Preview {
+    ScannerView()
+}
